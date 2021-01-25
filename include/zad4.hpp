17cce01124e5668af3dd05516acc80805920ca77
Jan Mikulevič <01145687@pw.edu.pl>
2021-01-25 16:51:54 +0100

    z4

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 // tutaj funkcja obliczMake
-void obliczMake(const std::vector<Tagliatelle> vec)
+double obliczMake(const std::vector<Tagliatelle> vec)
 {
     double M = 0.0;
     int    i = 1;
