66a82f44369ad30b4e3c4104870f02e49e78e07e
Jan Mikulevič <01145687@pw.edu.pl>
2021-01-25 16:49:18 +0100

    zad 4 zmiana VS

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -3,6 +3,15 @@
 #include <vector>
 
 // tutaj funkcja obliczMake
-void obliczMake(const std::vector<Tagliatelle> wek)
+void obliczMake(const std::vector<Tagliatelle> vec)
 {
+    double M = 0.0;
+    int    i = 1;
+    for (auto it = vec.crbegin(); it != vec.crend(); ++it)
+        M += it->ileMaki(i++);
+    if (M > 100)
+        throw 1;
+    else if (M > 50 && M <= 100)
+        throw 1.;
+    return M;
 }
